e9739956b1d6a73bfa7a25f223d52e4729561f03
JesusTalayeroP <jesus_talayero_apps1ma1920@cev.com>
2021-01-17 18:03:26 +0100

    documentated middleware->ensuretokens && userController

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -9,45 +9,53 @@ use App\Models\CardCollection;
 
 class CardController extends Controller
 {
+	/** POST
+	 * Crear cartas con cards/create
+	 *
+	 * Se introduce como parámetro (petición) el name de la carta, descripción y collection
+	 * a la que se va a ñadir la carta. En caso de no existir la collection se crea una nueva,
+	 * en caso de existir se añade la carta a la collection. Hace falta ser admin para poder
+	 * crear cartas, por lo que necesita estar logueado y enviar su token.
+	 *
+	 * @return response OK si se ha creado la carta 
+	 */
     public function create_card(Request $request) {
     	$response = "";
-
+    	//Leer el contenido de la petición
     	$data = $request->getContent();
-
+    	//Decodificar el json
     	$data = json_decode($data);
-
-    	
+    	// Buscar la collection
     	$collection = Collection::where('name', $data->collection)->get()->first();
-    	
-
+    	// Si existe la collection
     	if($data && $collection){
-
+    		// Crear la carta y rellenar los datos
     		$card = new Card();
     		$card->name = $data->name;
     		$card->description = $data->description;
     		
 			try{
+				// Guardar la carta
 				$card->save();
-				$card_id = $card->id;
 				$response = "OK";
 			}catch(\Exception $e){
 				$response = $e->getMessage();
 			}
-
+			// Añadir la carta a la collection
 			$cardCollection = new CardCollection();
 			$cardCollection->card_id = $card->id;
 			$cardCollection->collection_id = $collection->id;		
 			$cardCollection->save();
-
+		// Si no existe la collection
     	} elseif($data){
-
+    		// Crear la carta y rellenar los datos
     		$card = new Card();
     		$card->name = $data->name;
     		$card->description = $data->description;
-
+    		// Crear la collection y rellenar su nombre
     		$collection = new Collection();
     		$collection->name = $data->collection;
-
+    		// Guardar la card y collection
     		try{
 				$card->save();
 				$collection->save();
@@ -55,42 +63,46 @@ class CardController extends Controller
 			}catch(\Exception $e){
 				$response = $e->getMessage();
 			}
-
+			// Añadir la card a la collection
 			$cardCollection = new CardCollection();
 			$cardCollection->card_id = $card->id;
 			$cardCollection->collection_id = $collection->id;
 			$cardCollection->save();
 
     	} else $response = "Datos incorrectos";
-
+    	// Enviar la respuesta
     	return $response;
     }
 
-
+    /** POST
+	 * Actualizar cartas con cards/update/{id}
+	 *
+	 * Se reciben los parametros que queremos actualizar de la card y enviamos el id de la 
+	 * carta por la url. Hace falta ser admin para poder actualizar cartas, por lo
+	 * que necesita estar logueado y enviar su token.
+	 *
+	 * @param $id id de la carta que hay que actualizar
+	 * @return response OK si se ha actualizado la carta 
+	 */
     public function update_card(Request $request, $id){
 
 		$response = "";
-
-		//Buscar el equipo por su id
+		//Buscar la carta por su id
 		$card = Card::find($id);
-		// Si encuentra el equipo
+		// Si encuentra la carta
 		if($card){
-
 			//Leer el contenido de la petición
 			$data = $request->getContent();
-
 			//Decodificar el json
 			$data = json_decode($data);
-
-			//Si hay un json válido, buscar el equipo
+			//Si hay un json válido, buscar la carta
 			if($data){
-			
-				// Actualizar el nombre del equipo
+				// Actualizar elos datos de la carta
 				$card->name = (isset($data->name) ? $data->name : $card->name);
 				$card->description = (isset($data->description) ? $data->description : $card->description);
 
 				try{
-					// Guardar el equipo actualizado en la base de datos
+					// Guardar la carta actualizada en la base de datos
 					$card->save();
 					$response = "OK";
 				}catch(\Exception $e){
@@ -104,30 +116,35 @@ class CardController extends Controller
 		return response($response);
 	}
 
-
+	/** POST
+	 * Añadir cartas a las colecciones en card_collections_table con cards/add_card
+	 *
+	 * Se introduce como parámetro (petición) el id de la carta y el id de la collection a la
+	 * que se va a añadir la carta. Hace falta ser admin para poder añadir cartas a las
+	 * colecciones, por lo que necesita estar logueado y enviar su token.
+	 *
+	 * @return response OK si se ha añadido la carta a la coleccion
+	 */
 	public function add_card_to_collection(Request $request){
 
 		$response = "";
 		//Leer el contenido de la petición
 		$data = $request->getContent();
-
 		//Decodificar el json
 		$data = json_decode($data);
-
-		//Buscar el equipo por su id
+		//Buscar la carta por su id
 		$card = Card::find($data->card);
-		// Buscar el soldado que lidera el equipo por su id
+		// Buscar la coleccion a la que pertenece la carta por su id
 		$collection = Collection::find($data->collection);
-
-		//Si hay un json válido, añadir el soldado como lider del equipo
+		//Si hay un json válido, existe la carta y la coleccion
 		if($data && $card && $collection){
-
+			// añadir la carta a la coleccion
 			$cardCollection = new CardCollection();
 			$cardCollection->card_id = $data->card;
 			$cardCollection->collection_id = $data->collection;
 			
 			try{
-				// Guardar el equipo
+				// Guardar los datos
 				$cardCollection->save();
 				$response = "OK";
 			}catch(\Exception $e){
