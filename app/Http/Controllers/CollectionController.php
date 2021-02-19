c3256f9733d57de7dc95dd20d6c219d933a3fd20
jesusTalayeroP <jesus_talayero_apps1ma1920@cev.com>
2021-02-19 21:47:10 +0100

    CollectionController small fixes and validation BlackBox

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -33,52 +33,66 @@ class CollectionController extends Controller
         //Decodificar el token
         $headers = getallheaders();
         $decoded = JWT::decode($headers['api_token'], $key, array('HS256'));
-    	// Buscar la carta por su nombre
-    	$card = Card::where('name', $data->card)->get()->first();
-    	// Si existe la carta
-    	if($data && $card){
-    		// Crear la collection y rellenar sus datos
-    		$collection = new Collection();
-    		$collection->name = $data->name;
-    		$collection->icon = $data->icon;
-    		$collection->release_date = $data->release_date;
-    		
-			try{
-				$collection->save();
-				$response = "OK";
-			}catch(\Exception $e){
-				$response = $e->getMessage();
-			}
-			// Añadir la carta a la collection creada
-			$cardCollection = new CardCollection();
-			$cardCollection->card_id = $card->id;
-			$cardCollection->collection_id = $collection->id;		
-			$cardCollection->save();
-		// Si no existe la carta
-    	} elseif($data){
-    		// Crear la collection nueva
-    		$collection = new Collection();
-    		$collection->name = $data->name;
-    		$collection->icon = $data->icon;
-    		$collection->release_date = $data->release_date;
-    		// Crear la carta nueva
-    		$card = new Card();
-    		$card->name = $data->card;
-    		// Guardar los datos
-    		try{
-				$card->save();
-				$collection->save();
-				$response = "OK";
-			}catch(\Exception $e){
-				$response = $e->getMessage();
-			}
-			// Añadir la carta a la collection
-			$cardCollection = new CardCollection();
-			$cardCollection->card_id = $card->id;
-			$cardCollection->collection_id = $collection->id;
-			$cardCollection->save();
 
-    	} else $response = "Datos incorrectos";
+        if($data){
+        	if(!empty($data->name)){
+        		if(!empty($data->icon)){
+        			if(!empty($data->release_date)){
+        				$old_collection = Collection::where('name', $data->name)->get()->first();
+        				if(!$old_collection){
+        					if(!empty($data->card)){
+        						// Buscar la carta por su nombre
+	    						$card = Card::where('name', $data->card)->get()->first();
+	    						// Si existe la carta
+						    	if($card){
+						    		// Crear la collection y rellenar sus datos
+						    		$collection = new Collection();
+						    		$collection->name = $data->name;
+						    		$collection->icon = $data->icon;
+						    		$collection->release_date = $data->release_date;
+						    		
+									try{
+										$collection->save();
+										$response = "OK";
+									}catch(\Exception $e){
+										$response = $e->getMessage();
+									}
+									// Añadir la carta a la collection creada
+									$cardCollection = new CardCollection();
+									$cardCollection->card_id = $card->id;
+									$cardCollection->collection_id = $collection->id;		
+									$cardCollection->save();
+								// Si no existe la carta
+						    	} else{
+						    		// Crear la collection nueva
+						    		$collection = new Collection();
+						    		$collection->name = $data->name;
+						    		$collection->icon = $data->icon;
+						    		$collection->release_date = $data->release_date;
+						    		// Crear la carta nueva
+						    		$card = new Card();
+						    		$card->name = $data->card;
+						    		// Guardar los datos
+						    		try{
+										$card->save();
+										$collection->save();
+										$response = "OK";
+									}catch(\Exception $e){
+										$response = $e->getMessage();
+									}
+									// Añadir la carta a la collection
+									$cardCollection = new CardCollection();
+									$cardCollection->card_id = $card->id;
+									$cardCollection->collection_id = $collection->id;
+									$cardCollection->save();
+						    	}
+        					}else $response = "Colección incompleta, No card";
+        				}else $response = "Esa colección ya está registrada";
+        			}else $response = "Colección incompleta, No release date";
+        		}else $response = "Colección incompleta, No icon";
+        	}else $response = "Colección incompleta, No name";
+        }else $response = "No data";
+    	
     	// Enviar la respuesta
     	return $response;
     }
@@ -102,30 +116,45 @@ class CollectionController extends Controller
         $headers = getallheaders();
         $decoded = JWT::decode($headers['api_token'], $key, array('HS256'));
 
-		//Buscar la collection por su id
-		$collection = Collection::find($id);
-		// Si encuentra la collection
-		if($collection){
-			//Leer el contenido de la petición
-			$data = $request->getContent();
-			//Decodificar el json
-			$data = json_decode($data);
-			//Si hay un json válido, buscar la collection
-			if($data){
-				// Actualizar los datos de la collection
-				$collection->name = (isset($data->name) ? $data->name : $collection->name);
-				$collection->icon = (isset($data->icon) ? $data->icon : $collection->icon);
-				$collection->release_date = (isset($data->release_date) ? $data->release_date : $collection->release_date);
+        if(is_numeric($id)){
+        	//Buscar la collection por su id
+			$collection = Collection::find($id);
+			// Si encuentra la collection
+			if($collection){
+				//Leer el contenido de la petición
+				$data = $request->getContent();
+				//Decodificar el json
+				$data = json_decode($data);
+				//Si hay un json válido, buscar la collection
+				if($data){
+					if(!empty($data->name) || !empty($data->icon) || !empty($data->release_date)){
+						// Actualizar los datos de la collection
+						$collection->name = (isset($data->name) ? $data->name : $collection->name);
+						$collection->icon = (isset($data->icon) ? $data->icon : $collection->icon);
+						$collection->release_date = (isset($data->release_date) ? $data->release_date : $collection->release_date);
+						if(!empty($data->name)){
+							$old_collection = Collection::where('name', $data->name)->get()->first();
+						}else {
+							$old_collection = "";
+						}
+						
+						if(!$old_collection){
 
-				try{
-					// Guardar la collection actualizada en la base de datos
-					$collection->save();
-					$response = "OK";
-				}catch(\Exception $e){
-					$response = $e->getMessage();
-				}
-			} else $response = "Datos incorrectos";
-		} else $response = "No collection";
+							try{
+								// Guardar la collection actualizada en la base de datos
+								$collection->save();
+								$response = "OK";
+							}catch(\Exception $e){
+								$response = $e->getMessage();
+							}
+							
+						}else $response = "El nombre de la colección no puede ser el mismo que el de otra ya registrada.";
+					}else $response = "Debe introducir al menos un dato válido para poder actualizar la carta";
+				}else $response = "No data";
+			}else $response = "La colección no existe";
+        }else $response = "El id deben ser números";
+		
+		
 		
 		// Enviar respuesta
 		return response($response);
